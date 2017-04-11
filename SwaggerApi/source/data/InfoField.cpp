e81ab99f2d6d0a5e9304379d3ec00555c62bcc68
macias2k4 <maciejlamparski@gmail.com>
2017-04-11 22:19:53 +0100

    Swagger-Qt Finished: 1) Create class InfoField with properties 1.2) Add object of InfoField to Swagger class 2) Fill property of object swagger->info ( ) by SwaggerFiller

--- SwaggerApi/source/data/InfoField.cpp
+++ SwaggerApi/source/data/InfoField.cpp
@@ -1,6 +1,71 @@
 #include "InfoField.h"
 
-InfoField::InfoField(QObject *parent) : QObject(parent)
-{
+namespace Swagger {
+namespace Data {
 
+// ────────────────────────────────────────────────────────────────────────────────────────────── //
+InfoField::InfoField ( QObject *parent )
+    : Base::SwaggerFieldBase ( parent ) {
 }
+// ────────────────────────────────────────────────────────────────────────────────────────────── //
+InfoField::~InfoField() {
+}
+
+// ────────────────────────────────────────────────────────────────────────────────────────────── //
+//                                       Methods & Slots                                          //
+// ────────────────────────────────────────────────────────────────────────────────────────────── //
+
+// ────────────────────────────────────────────────────────────────────────────────────────────── //
+QString InfoField::title() const {
+    return _title;
+}
+// ────────────────────────────────────────────────────────────────────────────────────────────── //
+void InfoField::setTitle ( QString title ) {
+    if ( _title == title ) {
+        return;
+    }
+    _title = title;
+    emit titleChanged ( title );
+}
+
+// ────────────────────────────────────────────────────────────────────────────────────────────── //
+QString InfoField::description() const {
+    return _description;
+}
+// ────────────────────────────────────────────────────────────────────────────────────────────── //
+void InfoField::setDescription ( QString description ) {
+    if ( _description == description ) {
+        return;
+    }
+    _description = description;
+    emit descriptionChanged ( description );
+}
+
+// ────────────────────────────────────────────────────────────────────────────────────────────── //
+QString InfoField::termsOfService() const {
+    return _termsOfService;
+}
+// ────────────────────────────────────────────────────────────────────────────────────────────── //
+void InfoField::setTermsOfService ( QString termsOfService ) {
+    if ( _termsOfService == termsOfService ) {
+        return;
+    }
+    _termsOfService = termsOfService;
+    emit termsOfServiceChanged ( termsOfService );
+}
+
+// ────────────────────────────────────────────────────────────────────────────────────────────── //
+QString InfoField::version() const {
+    return _version;
+}
+// ────────────────────────────────────────────────────────────────────────────────────────────── //
+void InfoField::setVersion ( QString version ) {
+    if ( _version == version ) {
+        return;
+    }
+    _version = version;
+    emit versionChanged ( version );
+}
+
+} // Data
+} // Swagger
--- SwaggerApi/source/data/InfoField.h
+++ SwaggerApi/source/data/InfoField.h
@@ -1,17 +1,71 @@
 #ifndef INFOFIELD_H
 #define INFOFIELD_H
 
-#include <QObject>
+// ────────────────────────────────────────────────────────────────────────────────────────────── //
+// Swagger-Qt
+#include <SwaggerFieldBase.h>
 
-class InfoField : public QObject
-{
+namespace Swagger {
+namespace Data {
+
+/// \brief InfoField -> Info object, it is Swagger root object field
+class InfoField : public Base::SwaggerFieldBase {
     Q_OBJECT
+    Q_PROPERTY ( QString title READ title WRITE setTitle NOTIFY titleChanged )
+    Q_PROPERTY ( QString description READ description WRITE setDescription NOTIFY descriptionChanged )
+    Q_PROPERTY ( QString termsOfService READ termsOfService WRITE setTermsOfService NOTIFY
+                 termsOfServiceChanged )
+    Q_PROPERTY ( QString version READ version WRITE setVersion NOTIFY versionChanged )
+
 public:
-    explicit InfoField(QObject *parent = 0);
+    // ────────────────────────────────────────────────────────────────────────────────────────── //
+    // constructors
+    explicit InfoField ( QObject *parent = 0 );
+    ~InfoField ( );
+
+    // ────────────────────────────────────────────────────────────────────────────────────────── //
+    // methods
+
+    // - property
+
+    QString title ( ) const;
+    void setTitle ( QString title );
+
+    QString description ( ) const;
+    void setDescription ( QString description );
+
+    QString termsOfService ( ) const;
+    void setTermsOfService ( QString termsOfService );
+
+    QString version ( ) const;
+    void setVersion ( QString version );
 
 signals:
+    // ────────────────────────────────────────────────────────────────────────────────────────── //
+    // signals
+    void titleChanged ( QString title );
+    void descriptionChanged ( QString description );
+    void termsOfServiceChanged ( QString termsOfService );
+    void versionChanged ( QString version );
 
 public slots:
+    // ────────────────────────────────────────────────────────────────────────────────────────── //
+    // slots
+
+private:
+    // ────────────────────────────────────────────────────────────────────────────────────────── //
+    // property
+    /// \brief _title -> Required. The title of the application.
+    QString _title;
+    /// \brief _description -> A short description of the application
+    QString _description;
+    /// \brief _termsOfService -> The Terms of Service for the API
+    QString _termsOfService;
+    /// \brief _version -> Required Provides the version of the application API
+    QString _version;
 };
+
+} // Data
+} // Swagger
 
 #endif // INFOFIELD_H
