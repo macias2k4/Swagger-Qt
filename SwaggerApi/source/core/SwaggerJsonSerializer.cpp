bacaadd4c000a65ef838797db4ba0fcefad957ae
macias2k4 <maciejlamparski@gmail.com>
2017-04-30 22:08:15 +0100

    Swagger-Qt 1) Clearing code of adding parameters to operation 2) Creating and running SwaggerJsonSerializer class 3) Saving first QtSwagger.json file created from annotation in my projects 3.1) So far i don't have implemented operation Responses

--- SwaggerApi/source/core/SwaggerJsonSerializer.cpp
+++ SwaggerApi/source/core/SwaggerJsonSerializer.cpp
@@ -1,6 +1,131 @@
 #include "SwaggerJsonSerializer.h"
 
-SwaggerJsonSerializer::SwaggerJsonSerializer(QObject *parent) : QObject(parent)
-{
+namespace Swagger {
+namespace Core {
 
+// ────────────────────────────────────────────────────────────────────────────────────────────── //
+SwaggerJsonSerializer::SwaggerJsonSerializer ( Data::Swagger &swagger, QObject *parent )
+    : QObject  ( parent ),
+      _swagger ( &swagger ) {
 }
+// ────────────────────────────────────────────────────────────────────────────────────────────── //
+SwaggerJsonSerializer::~SwaggerJsonSerializer ( ) {
+}
+
+// ────────────────────────────────────────────────────────────────────────────────────────────── //
+//                                       Methods & Slots                                          //
+// ────────────────────────────────────────────────────────────────────────────────────────────── //
+
+// ────────────────────────────────────────────────────────────────────────────────────────────── //
+void SwaggerJsonSerializer::serialize ( ) {
+    _clearJsonObject ( _swaggerJson );
+    _createSwaggerRootJsonObject ( );
+}
+// ────────────────────────────────────────────────────────────────────────────────────────────── //
+void SwaggerJsonSerializer::_clearJsonObject ( QJsonObject &object ) {
+    QString key;
+    for ( int idx = object.keys ( ).size ( ) - 1 ; idx >= 0; idx-- ) {
+        key = object.keys ( ).at ( idx );
+        object.remove ( key );
+    }
+}
+// ────────────────────────────────────────────────────────────────────────────────────────────── //
+void SwaggerJsonSerializer::_createSwaggerRootJsonObject ( ) {
+    _swaggerJson.insert ( "swagger", _swagger->swagger ( ) );
+    _addInfoToSwaggerJson ( );
+    _swaggerJson.insert ( "host", _swagger->host ( ) );
+    _swaggerJson.insert ( "basePath", _swagger->basePath ( ) );
+    _addSchemesToSwaggerJson ( );
+    _addPathsToSwaggerJson ( );
+}
+// ────────────────────────────────────────────────────────────────────────────────────────────── //
+void SwaggerJsonSerializer::_addInfoToSwaggerJson ( ) {
+    _swaggerJson.insert ( "info", QJsonObject {
+        { "title", _swagger->info ( )->title ( ) },
+        { "description", _swagger->info ( )->description ( ) },
+        { "version", _swagger->info ( )->version ( ) }
+    } );
+}
+// ────────────────────────────────────────────────────────────────────────────────────────────── //
+void SwaggerJsonSerializer::_addSchemesToSwaggerJson ( ) {
+    QJsonArray schemes;
+    for ( QString scheme : _swagger->schemes ( ) ) {
+        schemes.append ( scheme );
+    }
+    _swaggerJson.insert ( "schemes", schemes );
+}
+// ────────────────────────────────────────────────────────────────────────────────────────────── //
+void SwaggerJsonSerializer::_addPathsToSwaggerJson ( ) {
+    _clearJsonObject ( _pathsJson );
+    for ( Base::OperationFieldBase *operation : _swagger->operations ( ) ) {
+        _currentOperation = operation;
+        _addOperationToPathsJson ( );
+    }
+    _swaggerJson.insert ( "paths", _pathsJson );
+}
+// ────────────────────────────────────────────────────────────────────────────────────────────── //
+void SwaggerJsonSerializer::_addOperationToPathsJson ( ) {
+    if ( !_currentOperation ) {
+        qWarning ( ) << "Can't add single operation to paths";
+        return;
+    }
+    QJsonValue pathValue = _pathsJson.take ( _currentOperation->path ( ) );
+    QJsonObject pathJson = pathValue.toObject ( QJsonObject ( ) );
+    QJsonObject operationJson = _createOperationJson ( );
+    pathJson.insert ( Base::OperationFieldBase::operationTypeValueToString (
+                          _currentOperation->operationType ( ) ).toLower ( ), operationJson );
+    _pathsJson.insert ( _currentOperation->path ( ), pathJson );
+}
+// ────────────────────────────────────────────────────────────────────────────────────────────── //
+QJsonObject SwaggerJsonSerializer::_createOperationJson ( ) {
+    QJsonObject operationJson {
+        { "description", _currentOperation->description ( ) },
+        { "summary", _currentOperation->summary ( ) },
+        { "operationId", _currentOperation->operationId ( ) }
+    };
+    // clear code - sub methods !
+    QJsonArray tags;
+    for ( QString tag : _currentOperation->tags ( ) ) {
+        tags.append ( tag );
+    }
+    operationJson.insert ( "tags", tags );
+    QJsonArray consumes;
+    for ( QString consume : _currentOperation->consumes ( ) ) {
+        consumes.append ( consume );
+    }
+    operationJson.insert ( "consumes", consumes );
+    QJsonArray produces;
+    for ( QString produce : _currentOperation->produces ( ) ) {
+        produces.append ( produce );
+    }
+    operationJson.insert ( "produces", produces );
+    QJsonArray schemes;
+    for ( QString scheme : _currentOperation->schemes ( ) ) {
+        schemes.append ( scheme );
+    }
+    operationJson.insert ( "schemes", schemes );
+    QJsonArray parameters;
+    for ( Base::ParameterFieldBase *parameter : _currentOperation->parameters ( ) ) {
+        if ( parameter ) {
+            // depending on 'in' value different extending parameters
+            parameters.append ( QJsonObject {
+                { "name", parameter->name ( ) },
+                { "in", parameter->in ( ) },
+                { "description", parameter->description ( ) },
+                { "required", parameter->required ( ) }
+            } );
+        }
+    }
+    operationJson.insert ( "parameters", parameters );
+    return operationJson;
+}
+
+// ────────────────────────────────────────────────────────────────────────────────────────────── //
+// property
+QJsonObject SwaggerJsonSerializer::swaggerJson ( ) const {
+    return _swaggerJson;
+}
+
+
+} // Core
+} // Swagger
--- SwaggerApi/source/core/SwaggerJsonSerializer.h
+++ SwaggerApi/source/core/SwaggerJsonSerializer.h
@@ -1,17 +1,77 @@
 #ifndef SWAGGERJSONSERIALIZER_H
 #define SWAGGERJSONSERIALIZER_H
 
+// ────────────────────────────────────────────────────────────────────────────────────────────── //
+// Qt
 #include <QObject>
+#include <QMetaProperty>
+#include <QJsonObject>
+#include <QJsonArray>
 
-class SwaggerJsonSerializer : public QObject
-{
+// ────────────────────────────────────────────────────────────────────────────────────────────── //
+// Swagger-Qt
+#include <Swagger.h>
+
+namespace Swagger {
+namespace Core {
+
+class SwaggerJsonSerializer : public QObject {
     Q_OBJECT
 public:
-    explicit SwaggerJsonSerializer(QObject *parent = 0);
+    // ────────────────────────────────────────────────────────────────────────────────────────── //
+    // constructors
+    explicit SwaggerJsonSerializer ( Data::Swagger &swagger, QObject *parent = 0 );
+    ~SwaggerJsonSerializer ( );
+
+    // ────────────────────────────────────────────────────────────────────────────────────────── //
+    // methods
+    /// \brief serialize -> serialize actual _swagger object to json object
+    void serialize ( );
+
+    // - property
+    /// \brief swaggerJson -> return swagger json object
+    QJsonObject swaggerJson ( ) const;
 
 signals:
+    // ────────────────────────────────────────────────────────────────────────────────────────── //
+    // signals
 
 public slots:
+    // ────────────────────────────────────────────────────────────────────────────────────────── //
+    // slots
+
+private:
+    // ────────────────────────────────────────────────────────────────────────────────────────── //
+    // property
+    /// \brief _swagger -> root object of swagger document
+    Data::Swagger *_swagger = nullptr;
+    /// \brief _swaggerJson -> swagger json object
+    QJsonObject _swaggerJson;
+    /// \brief _pathsJson -> objest with paths
+    QJsonObject _pathsJson;
+    /// \brief _currentOperation -> operation currently added to json
+    Base::OperationFieldBase *_currentOperation = nullptr;
+
+    // ────────────────────────────────────────────────────────────────────────────────────────── //
+    // methods
+    /// \brief _clearJsonObject -> clearing passed json object
+    void _clearJsonObject ( QJsonObject &object );
+    /// \brief _createSwaggerRootJsonObject -> creating root swagger json object
+    void _createSwaggerRootJsonObject ( );
+    /// \brief _addInfoToSwaggerJson -> add info to swagger json object
+    void _addInfoToSwaggerJson ( );
+    /// \brief _addSchemesToSwaggerJson -> add schemes to swagger json object
+    void _addSchemesToSwaggerJson ( );
+    /// \brief _addPathsToSwaggerJson -> add paths to swagger json object
+    void _addPathsToSwaggerJson ( );
+    /// \brief _addOperationToPathsJson -> add single operation for paths in swagger
+    void _addOperationToPathsJson ( );
+    /// \brief _createOperationJson -> creating operation json object
+    QJsonObject _createOperationJson ( );
+
 };
+
+} // Core
+} // Swagger
 
 #endif // SWAGGERJSONSERIALIZER_H
